fcbb9dbf88d5b3b9bd637f14c24222f6fdc81a76
Tianding_hou <Apollohoutianding@outlook.com>
2025-06-21 12:20:05 +0200

    update on cli rahmenprogramm, implemented memory access

--- include/secure-Memory-Unit.hpp
+++ include/secure-Memory-Unit.hpp
@@ -1,1 +1,66 @@
 //TODO
+#ifndef SECURE_MEMORY_UNIT_HPP
+#define SECURE_MEMORY_UNIT_HPP
+
+#include <systemc>
+#include <systemc.h>
+#include <map>
+using namespace sc_core;
+
+#include "address_Scrambler.hpp"
+#include "encryptor.hpp"
+#include "fault-Injection.hpp"
+#include "memory-Access.hpp"
+#include "Parity-Checker.hpp"
+#include "prng.hpp"
+
+SC_MODULE(TINY_RISC) {
+
+
+  SC_HAS_PROCESS(TINY_RISC);
+
+    ADDRESS_SCRAMBLER address_scrambler;
+    ENCRYPTOR encryptor;
+    FAULT_INJECTION fault_injection;
+    MEMORY_ACCESS memory_access;
+    PARITY_CHECKER parity_checker;
+    PRNG prng;
+
+
+
+  std::map<unsigned int, unsigned int>& init_mem;
+
+
+    sc_clock clk;
+    sc_in<uint32_t> addr;
+    sc_in<uint32_t> wdata;
+    sc_in<bool> r;
+    sc_in<bool> w;
+    sc_in<uint32_t> fault;
+    sc_in<sc_bv<4>> faultbit;
+
+
+    sc_out<uint32_t> rdata;
+    sc_out<bool> ready;
+    sc_in<bool> error;
+
+
+
+  
+  //TODO
+
+  SMU(sc_module_name name,
+          uint8_t endianness, uint32_t latency_scrambling, uint32_t latency_encryption,
+          uint32_t latency_memory_access,uint32_t seed)
+    : 
+    sc_module(name),
+    
+    {
+    
+
+   
+}
+
+};
+
+#endif // TINYRISC_HPP
--- src/main.c
+++ src/main.c
@@ -11,6 +11,11 @@ const char* usage_msg =
     "Options:\n"
     "   -c,--cycles N: uint32_t        Number of cycles (default: 1)\n"
     "   -t,--tf : String               Path to the TraceFile\n"
+    "   -e, --endianness N:uint8_t            Endianness: 0=Little-Endian, 1=Big-Endian(default:0)\n"
+    "   -s, --latency-scrambling N:uint32_t    Latency for Address Scrambling in clock cycles(default:0)\n"
+    "   -n, --latency-encrypt N:uint32_t       Latency for Encryption/Decryption in clock cycles(default:0)\n"
+    "   -m, --latency-memory-access N:uint32_t Latency for Memory Access in clock cycles(default:0)\n"
+    "   -d, --seed N:uint32_t                  Seed for the pRNG\n"
     "   -h,--help: flag                Show the help message\n";
 
 const char* help_msg =
@@ -18,10 +23,14 @@ const char* help_msg =
     "  <file>  The path to input file:list of requests\n"
     "\n"
     "Optional arguments:\n"
-    "  --cycles: uint32_t   The number of cycles (default: 1)\n"
-    "  --tf: String          Path to the TraceFile\n"
-    "  --help: flag          Show the help message\n";
-
+    "  -c, --cycles N                Number of cycles (uint32_t, default: 1)\n"
+    "  -t, --tf PATH                 Path to the TraceFile (string)\n"
+    "  -e, --endianness N            Endianness (uint8_t): 0=Little-Endian, 1=Big-Endian\n"
+    "  -s, --latency-scrambling N    Latency for Address Scrambling (uint32_t)\n"
+    "  -n, --latency-encrypt N       Latency for Encryption/Decryption (uint32_t)\n"
+    "  -m, --latency-memory-access N Latency for Memory Access (uint32_t)\n"
+    "  -d, --seed N                  Seed for pRNG (uint32_t)\n"
+    "  -h, --help                    Show this help message\n";
 void print_usage(const char* progname) {
     fprintf(stderr, usage_msg, progname);
 }
@@ -32,21 +41,47 @@ void print_help(const char* progname) {
 }
 
 
-int parse_cli(int argc, char *argv[], uint32_t *cycles, char **tracefile, char **input_file) {  
+int parse_cli(int argc, char *argv[], uint32_t *cycles, char **tracefile, char **input_file,
+              uint8_t *endianness, uint32_t *latency_scrambling, uint32_t *latency_encrypt,
+              uint32_t *latency_memory_access, uint32_t *seed) {  
     static struct option long_options[] = {
         {"cycles", required_argument, 0, 'c'},
         {"tf", required_argument, 0, 't'},
+        {"endianness", required_argument, 0, 'e'},
+        {"latency-scrambling", required_argument, 0, 's'},
+        {"latency-encrypt", required_argument, 0, 'n'},
+        {"latency-memory-access", required_argument, 0, 'm'},
+        {"seed", required_argument, 0, 'd'},
         {"help", no_argument, 0, 'h'},
         {0, 0, 0, 0}
     };
     int opt;
-    while ((opt = getopt_long(argc,argv,"c:t:h", long_options, NULL)) != -1) {
+    while ((opt = getopt_long(argc,argv,"c:t:e:s:n:m:d:h", long_options, NULL)) != -1) {
         switch (opt) {
             case 'c':
                 *cycles = (uint32_t)strtoul(optarg, NULL, 10);
                 break;
             case 't':
                 *tracefile = optarg;
+                break;
+            case 'e':
+                *endianness = (uint8_t)strtoul(optarg, NULL, 10);
+                if (*endianness != 0 && *endianness != 1) {
+                    fprintf(stderr, "Invalid endianness value. Use 0 for Little-Endian or 1 for Big-Endian.\n");
+                    return EXIT_FAILURE;
+                }
+                break;
+            case 's':
+                *latency_scrambling = (uint32_t)strtoul(optarg, NULL, 10);
+                break;
+            case 'n':
+                *latency_encrypt = (uint32_t)strtoul(optarg, NULL, 10);
+                break;
+            case 'm':
+                *latency_memory_access = (uint32_t)strtoul(optarg, NULL, 10);
+                break;
+            case 'd':
+                *seed = (uint32_t)strtoul(optarg, NULL, 10);
                 break;
             case 'h':
                 print_help(argv[0]);
@@ -97,8 +132,15 @@ int main(int argc, char** argv){
     static uint32_t cycles = 1;
     static char *tracefile = NULL;
     static char *input_file = NULL;
+    static uint8_t endianness = 0; // Default: Little-Endian
+    static uint32_t latency_scrambling = 0; // Default: 0
+    static uint32_t latency_encrypt = 0; // Default: 0
+    static uint32_t latency_memory_access = 0; // Default: 0
+    static uint32_t seed = 0; // Default: 0
 
-    int result = parse_cli(argc, argv, &cycles , &tracefile, &input_file);
+    int result = parse_cli(argc, argv, &cycles , &tracefile, &input_file,
+                           &endianness, &latency_scrambling, &latency_encrypt,
+                           &latency_memory_access, &seed);
     if (result != 0 || input_file == NULL) {
         return result;
     }
@@ -122,6 +164,7 @@ int main(int argc, char** argv){
     fread(input, 1, file_size, file);
     fclose(file);
     input[file_size] = '\0';
+    //TODO
 
     return 0;
 }
