411d3747e29e8a271a1e81c75706f01cd755730e
Tianding_hou <Apollohoutianding@outlook.com>
2025-06-29 17:31:51 +0200

    added build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
 echo "Test script start!"
+echo "##  Build started at $(date)"
+make clean
+make all
+./project ./test/parse_inout_file/eingabe/eingabe.csv
--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <stdint.h>
 #include <errno.h>
+#include <ctype.h>
 
 struct Request {
     uint32_t addr;
@@ -341,7 +342,11 @@ int main(int argc, char** argv){
     uint32_t numRequests = 0;
     struct Request *requests = NULL;
 
-    result = parse_requests(input, &numRequests, &requests);
+    if (parse_requests(input, &numRequests, &requests) != 0) {
+        fprintf(stderr, "Failed to parse requests from input file.\n");
+        free(input);
+        return 1;
+    }
 
     run_simulation(
                    cycles,
